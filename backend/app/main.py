950790c852a4e1a9053398f5bb15a6167ecb0c70
SanchitPandey26 <sanchitpandey263@gmail.com>
2025-11-24 17:14:31 +0530

    I am going to die

--- backend/app/main.py
+++ backend/app/main.py
@@ -17,11 +17,7 @@ app = FastAPI(
 # ---------------------------------------------------------
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:3000",
-        "http://localhost:3001",
-        "https://shl-assessment-recommendation-eight.vercel.app/"
-    ],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
