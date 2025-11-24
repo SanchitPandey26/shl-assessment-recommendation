3a77eecca9b593b6ae650f74de1d3ccc85984702
SanchitPandey26 <sanchitpandey263@gmail.com>
2025-11-24 15:56:47 +0530

    please final

--- backend/start.sh
+++ backend/start.sh
@@ -1,2 +1,9 @@
-#!/bin/bash
+#!/usr/bin/env bash
+set -e
+
+echo ">>> START.SH EXECUTED <<<"
+
+# Ensure file permission is correct even on Windows commits
+chmod +x start.sh
+
 uvicorn app.main:app --host 0.0.0.0 --port $PORT
